5f092e43f10f990d627035ea2c32518c5ef0afb9
xiaole <moxiaoxiao0816@163.com>
2022-05-17 14:52:11 +0800

    init project

---
+++ assets/js/login.js
@@ -0,0 +1,61 @@
+$(function() {
+    $('#go-reg').on('click', function() {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    
+    $('#go-login').on('click', function() {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    let form = layui.form
+    let layer = layui.layer
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ],
+        repass: function(value) {
+            let val = $('.reg-box [name=password]').val()
+            if(val !== value){
+                return '两次密码不一致'
+            }
+        }
+    })
+    //监听注册表单的提交事件
+    $('#form-reg').on('submit', function(e) {
+        e.preventDefault()
+       
+        $.post('http://www.liulongbin.top:3007/api/reguser',{
+            username: $('#form-reg [name=username]').val(),password: $('#form-reg [name=password]').val()},function(res) {
+                if(res.status !== 0){
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功，请登录')
+                console.log($('#form-reg [name=username]').val())
+                // 注册成功后  自动跳转到登录界面
+                $('#go-login').click()
+            }
+        )
+    
+    })
+    // 监听登录表单的提交事件
+    $('#form-login').on('submit', function(e) {
+        e.preventDefault()
+        
+        $.post('http://www.liulongbin.top:3007/api/login',{
+            username: $('#form-login [name=username]').val(),password: $('#form-login [name=password]').val()},function(res) {
+                if(res.status !== 0){
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功')
+                localStorage.setItem('token',res.token)
+                location.href = '/momo.html'
+                
+            }
+        )
+        
+    })
+   
+    
+})
